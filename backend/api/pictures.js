fea187a8a9c42a6823ea0332baa62f7917dde802
Alex <alex1mosh@gmail.com>
2020-11-06 01:16:04 +0200

    Add few models

--- backend/api/pictures.js
+++ backend/api/pictures.js
@@ -1,56 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Picture= require('../models/Pictures');
+const Picture = require("../models/Pictures");
 //get all products
-router.get("/",(req,res)=>{
-    Picture.find().then(data=>{
-        res.status(200).json({
-            pictures:data
-        })
-    })
-
+router.get("/", (req, res) => {
+  Picture.find().then((data) => {
+    res.status(200).json({
+      pictures: data,
+    });
+  });
 });
 
-router.post("/",(req,res)=>{
-
-    
-    const picture=new Picture({
-        catalogNumber:req.body.catalogNumber,
-        name:req.body.name,
-        price:req.body.price,
-        imgPath:req.body.imgPath,
-        description:req.body.description,
-        size:req.body.size,
-        inStock:req.body.inStock
-    })
-    picture.save().then(data=>{
-           res.status(200).json({
-              picture:data
-           });
-       })
-    
+router.post("/", (req, res) => {
+  const picture = new Picture({
+    catalogNumber: req.body.catalogNumber,
+    name: req.body.name,
+    price: req.body.price,
+    imgPath: req.body.imgPath,
+    description: req.body.description,
+    size: req.body.size,
+    inStock: req.body.inStock,
+  });
+  picture.save().then((data) => {
+    res.status(200).json({
+      picture: data,
+    });
+  });
 });
 
-router.put("/:_id",(req,res)=>{
-  
-    Picture.findOneAndUpdate({_id:req.params._id},req.body).then(data=>{
-       res.status(200).json({
-           picture:data
-       })
-    })
-   res.json({success:"updateComplete"})
-})
+router.put("/:_id", (req, res) => {
+  Picture.findOneAndUpdate({ _id: req.params._id }, req.body).then((data) => {
+    res.status(200).json({
+      picture: data,
+    });
+  });
+  res.json({ success: "updateComplete" });
+});
 
-router.get("/:_id",(req,res)=>{
-    Picture.findById({_id:req.params._id}).then(data=>{
-        res.json({picture:data});
-    })
-})
+router.get("/:_id", (req, res) => {
+  Picture.findById({ _id: req.params._id }).then((data) => {
+    res.json({ picture: data });
+  });
+});
 
-router.delete("/:_id",(req,res)=>{
-    Picture.findByIdAndRemove({_id:req.params._id}).then(data=>{
-        res.json({data:data})
-    })
-})
+router.delete("/:_id", (req, res) => {
+  Picture.findByIdAndRemove({ _id: req.params._id }).then((data) => {
+    res.json({ data: data });
+  });
+});
 
 module.exports = router;
--- backend/models/Pictures.js
+++ backend/models/Pictures.js
@@ -1,16 +1,13 @@
-const mongoose =require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Picture = new Schema({
-
-   
-    catalogNumber:String,
-    name:String,
-    price:String,
-    imgPath:String,
-    description:String,
-    size:String,
-    inStock:String
-
+  catalogNumber: String,
+  name: String,
+  price: String,
+  imgPath: String,
+  description: String,
+  size: String,
+  inStock: String,
 });
 
-module.exports = mongoose.model("pictures",Picture);
+module.exports = mongoose.model("pictures", Picture);
