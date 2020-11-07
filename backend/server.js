bb2b39c93118281175aa5c618e05da1ee046e087
Alex <alex1mosh@gmail.com>
2020-11-07 06:07:31 +0200

    add few commits

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,15 @@ app.use(cors());
 const keys = require("./config/keys");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+
+
 //connected to server in port 4500
 app.listen(PORT, () => {
   console.log(`connected to server complete with port: ${PORT}`);
 });
+
+
+//connect to mongoDb 
 mongoose
   .connect(keys.mongodb.dbURI, { useNewUrlParser: true })
   .then((result) => {
@@ -24,7 +29,9 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//routers
 app.use(pictureRoute);
