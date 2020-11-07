f51ea8f3283e6e5f69ba13277a6def1541cf0471
Alex <alex1mosh@gmail.com>
2020-11-07 06:05:07 +0200

    route fix

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,30 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const app =express();
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
 const PORT = 4000;
 
-const cors =require('cors');
+const pictureRoute = require("./routes/pictures");
+
+const cors = require("cors");
 app.use(cors());
-const keys = require('./config/keys');
-const bodyParser = require('body-parser');
+const keys = require("./config/keys");
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 //connected to server in port 4500
-app.listen(PORT,()=>{
-    console.log(`connected to server complete with port: ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`connected to server complete with port: ${PORT}`);
 });
-mongoose.connect(keys.mongodb.dbURI,{useNewUrlParser:true}).then((result) => {
-    if(result){
-        console.log("connected to mongo complete");
+mongoose
+  .connect(keys.mongodb.dbURI, { useNewUrlParser: true })
+  .then((result) => {
+    if (result) {
+      console.log("connected to mongo complete");
     }
-   
-}).catch((err) => {
-  console.log(err);
-});
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.urlencoded({extended:false}))
-const pictures = require('../backend/api/pictures');
- app.use("/pictures",pictures);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
+app.use(pictureRoute);
