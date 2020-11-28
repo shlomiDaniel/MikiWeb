a729a8fdb044ebc899352e9f0c50af25443c761b
Alex <alex1mosh@gmail.com>
2020-11-28 05:47:23 +0200

    admin page tab fix

--- frontend/mikiWeb/src/app/app-routing.module.ts
+++ frontend/mikiWeb/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminPageComponent } from './users/user/admin-page/admin-page.component';
+import { OrderViewComponent } from './users/user/admin-page/order-view/order-view.component';
+import { PictureAddComponent } from './users/user/admin-page/picture-add/picture-add.component';
 import { LoginComponent } from './users/user/auth/login/login.component';
 import { RegistrationComponent } from './users/user/auth/registration/registration.component';
 
@@ -10,7 +12,14 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'registration', component: RegistrationComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'admin', component: AdminPageComponent}
+  {
+    path: 'admin',
+    component: AdminPageComponent,
+    children: [
+      { path: 'addproduct', component: PictureAddComponent },
+      { path: 'orderview', component: OrderViewComponent },
+    ],
+  },
 ];
 
 @NgModule({
