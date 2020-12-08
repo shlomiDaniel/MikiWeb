71e1a41f276a56140e252be76b4e6d6ccd5a1937
Alex <alex1mosh@gmail.com>
2020-12-08 06:13:19 +0200

    nav-bar mobile fix

--- frontend/mikiWeb/src/app/app.component.ts
+++ frontend/mikiWeb/src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'mikiWeb';
+  innerWidth: any;
+
+  getStyleList() {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth > 450) {
+      return { 'margin-top': '4%' };
+    } else {
+      return { 'margin-top': '15%' };
+    }
+  }
 }
